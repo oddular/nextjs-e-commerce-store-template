39ade663fb0abd5f19282ce103d8d9dfe6df4b8a
mknapper1 <mknapper1@gmail.com>
2021-03-03 16:57:38 -0500

    wip

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Deploy Now
 
-## Getting Started
+[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/git/external?repository-url=https%3A%2F%2Fgithub.com%2Foddular%2Fodd-shop.git&env=ODDULAR_PUBLIC_SHOP_KEY&envDescription=Api%20Key%20needed%20to%20link%20to%20Oddular)
+
+## Running locally
 
 First, run the development server:
 
@@ -26,9 +28,3 @@ To learn more about Next.js, take a look at the following resources:
 - [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
 
 You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
--- pages/index.js
+++ pages/index.js
@@ -1,53 +1,75 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { useEffect, useState } from 'react';
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+import Spinner from '../components/spinner';
+
+import { OddularStorefrontClient, gql } from '@oddular/commerce-core';
 
 export default function Home() {
+  const [d, setD] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const OddularClient = new OddularStorefrontClient(
+      'ODDULAR_PUBLIC_TOKEN_Nkpn4ZAmPJZD1Gs7SyiAP8CjQ5GRF6',
+      'http://localhost:8000/storefront/',
+      true,
+      {},
+    );
+
+    const productFragment = gql`
+      fragment ProductFragment on Product {
+        id
+        name
+      }
+    `;
+
+    OddularClient.getProductList({ first: 5 }, productFragment)
+      .then((data) => {
+        setD(data);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Weird Store</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Weird Store!</a>
         </h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        {!!loading ? (
+          <Spinner />
+        ) : (
+          <>
+            <div className={styles.grid}>
+              {d.products.edges.map((node) => {
+                let product = node.node;
+                return (
+                  <a
+                    href="https://nextjs.org/docs"
+                    className={styles.card}
+                    key={product.id}
+                  >
+                    <h3>{product.name} &rarr;</h3>
+                  </a>
+                );
+              })}
+            </div>
+          </>
+        )}
       </main>
 
       <footer className={styles.footer}>
@@ -56,10 +78,9 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          Powered by Oddular
         </a>
       </footer>
     </div>
-  )
+  );
 }
