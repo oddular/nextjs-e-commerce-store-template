a9188ce3046252da4b4ba5ad8241f52c0e832455
Michael <mkaufmann421@gmail.com>
2021-05-03 15:45:25 -0400

    Implement Category and Collection Meta components

--- components/OddularCategoryMeta.tsx
+++ components/OddularCategoryMeta.tsx
@@ -3,10 +3,62 @@ import Head from 'next/head';
 
 const preFetch = (id: string, storeKey: string) => {};
 
-interface OddularCategoryMetaProps {}
+const getDescription: any = (category: any) => {
+  //seo.og_description > seo.description > description
+  if(!!category.seo){
+    if(!!category.seo.og_description){
+      return category.seo.og_description;
+    }else if(!!category.seo.description){
+      return category.seo.description;
+    }
+  }
+  return category.description;
+}
 
-const OddularCategoryMeta: React.FC<OddularCategoryMetaProps> = ({}) => {
-  return <></>;
+const getTitle: any = (category: any) => {
+  //seo.og_title > seo.title > name
+  if(!!category.seo){
+    if(!!category.seo.og_title){
+      return category.seo.og_title;
+    }else if(!!category.seo.title){
+      return category.seo.title;
+    }
+  }
+  return category.name;
+}
+
+const getImage: any = (category: any) => {
+  //seo.image > thumbnail.url > static oddular logo
+  if(!!category.seo){
+    if(!!category.seo.og_title){
+      return category.seo.og_title;
+    }else if(!!category.seo.title){
+      return category.seo.title;
+    }
+  }
+  return "https://via.placeholder.com/200/8D43F0/FFFFFF/?text=Oddular";
+}
+
+interface OddularCategoryMetaProps {
+    category: any
+}
+
+const OddularCategoryMeta: React.FC<OddularCategoryMetaProps> = ({category}) => {
+  return <>
+      {/* Twitter */}
+      <meta name="twitter:card" content="summary" key="twcard" />
+      <meta name="twitter:creator" content="oddularhq" key="twhandle" />
+
+      {/* Open Graph */}
+
+      {/* <meta property="og:url" content={currentURL} key="ogurl" /> */}
+
+      <meta property="og:image" content={getImage(category)} key="ogimage" />
+      <meta property="og:type" content="website"/>
+      {/* <meta property="og:site_name" content={} key="ogsitename" /> */}
+      <meta property="og:title" content={getTitle(category)} key="ogtitle" />
+      <meta property="og:description" content={getDescription(category)} key="ogdesc" />
+  </>;
 };
 
 export default OddularCategoryMeta;
--- components/OddularCollectionMeta.tsx
+++ components/OddularCollectionMeta.tsx
@@ -3,10 +3,60 @@ import Head from 'next/head';
 
 const preFetch = (id: string, storeKey: string) => {};
 
-interface OddularCollectionMetaProps {}
+const getDescription: any = (collection: any) => {
+  //seo.og_description > seo.description > description
+  if(!!collection.seo){
+    if(!!collection.seo.og_description){
+      return collection.seo.og_description;
+    }else if(!!collection.seo.description){
+      return collection.seo.description;
+    }
+  }
+  return collection.description;
+}
 
-const OddularCollectionMeta: React.FC<OddularCollectionMetaProps> = ({}) => {
-  return <></>;
+const getTitle: any = (collection: any) => {
+  //seo.og_title > seo.title > name
+  if(!!collection.seo){
+    if(!!collection.seo.og_title){
+      return collection.seo.og_title;
+    }else if(!!collection.seo.title){
+      return collection.seo.title;
+    }
+  }
+  return collection.name;
+}
+
+const getImage: any = (collection: any) => {
+  //seo.image > thumbnail.url > static oddular logo
+  if(!!collection.seo){
+    if(!!collection.seo.og_title){
+      return collection.seo.og_title;
+    }else if(!!collection.seo.title){
+      return collection.seo.title;
+    }
+  }
+  return "https://via.placeholder.com/200/8D43F0/FFFFFF/?text=Oddular";
+}
+
+interface OddularCollectionMetaProps {collection: any}
+
+const OddularCollectionMeta: React.FC<OddularCollectionMetaProps> = ({collection}) => {
+  return <>
+      {/* Twitter */}
+      <meta name="twitter:card" content="summary" key="twcard" />
+      <meta name="twitter:creator" content="oddularhq" key="twhandle" />
+
+      {/* Open Graph */}
+
+      {/* <meta property="og:url" content={currentURL} key="ogurl" /> */}
+
+      <meta property="og:image" content={getImage(collection)} key="ogimage" />
+      <meta property="og:type" content="website"/>
+      {/* <meta property="og:site_name" content={} key="ogsitename" /> */}
+      <meta property="og:title" content={getTitle(collection)} key="ogtitle" />
+      <meta property="og:description" content={getDescription(collection)} key="ogdesc" />
+  </>;
 };
 
 export default OddularCollectionMeta;
